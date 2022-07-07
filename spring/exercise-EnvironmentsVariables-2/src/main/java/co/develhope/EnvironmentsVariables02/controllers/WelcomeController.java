4961b91d0468fba30f4ffea32bf08f437b28efa7
Alessandro Canulli <canullialessandro@icloud.com>
2022-07-07 11:38:59 +0200

    .

---
+++ spring/exercise-EnvironmentsVariables-2/src/main/java/co/develhope/EnvironmentsVariables02/controllers/WelcomeController.java
@@ -0,0 +1,19 @@
+package co.develhope.EnvironmentsVariables02.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("")
+    public String mainEndpoint(){
+        return environment.getProperty("myCustomEnvs.welcomeMsg");
+    }
+
+}
